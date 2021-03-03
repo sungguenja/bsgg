bf79542b1b5b1ec5759129bc26e7b7e09445300b
sungguenja <59605197+sungguenja@users.noreply.github.com>
2021-03-03 21:21:54 +0900

    add adella |front/src/components/gamedata/SkillWindow.vue| add adella

--- front/src/components/gamedata/SkillWindow.vue
+++ front/src/components/gamedata/SkillWindow.vue
@@ -21,6 +21,7 @@
             <img v-if="chname == '키아라' && button == 'R'" src="https://raw.githubusercontent.com/sungguenja/lumiaimg/master/스킬/키아라/after_R.png" style="height:80px;">
             <img v-if="chname == '쇼이치' && button == 'Q'" src="https://raw.githubusercontent.com/sungguenja/lumiaimg/master/스킬/쇼이치/after_Q.png" alt="" style="height:80px;">
             <img v-if="chname == '루크' && button == 'Q'" src="https://raw.githubusercontent.com/sungguenja/lumiaimg/master/스킬/루크/after_Q.png" alt="" style="height:80px;">
+            <img v-if="chname == '아델라' && button == 'Q'" src="https://raw.githubusercontent.com/sungguenja/lumiaimg/master/스킬/아델라/after_Q.png">
             <br><b-button v-b-toggle="button+button" class="m-1">스킬<br>상세정보</b-button>
           </td>
           <td>
@@ -43,6 +44,15 @@
             </div>
             <div v-if="chname == '캐시' && button =='R'">
               <img style="width: 50%;" src="https://raw.githubusercontent.com/sungguenja/lumiaimg/master/스킬설명/캐시/basic_R2.gif">
+            </div>
+            <div v-if="chname == '아델라' && button =='패시브'">
+              <img style="width: 50%;" src="https://raw.githubusercontent.com/sungguenja/lumiaimg/master/스킬설명/아델라/basic_패시브2.gif">
+            </div>
+            <div v-if="chname == '아델라' && button =='Q'">
+              <img style="width: 50%;" src="https://raw.githubusercontent.com/sungguenja/lumiaimg/master/스킬설명/아델라/after_Q.gif">
+            </div>
+            <div v-if="chname == '아델라' && button =='R'">
+              <img style="width: 50%;" src="https://raw.githubusercontent.com/sungguenja/lumiaimg/master/스킬설명/아델라/basic_R2.gif">
             </div>
             <a v-html="detailText"></a>
           </td>
